703460f8434e38845e22b2d68c2b4424dfd6afd3
bohdana_kuzmenko <bogdana.kuzmenko.16@gmail.com>
2025-05-26 18:11:52 +0200

    ✨Enhance Loader component with dynamic label updates and improved composition

--- src/sourcerer/presentation/screens/shared/widgets/loader.py
+++ src/sourcerer/presentation/screens/shared/widgets/loader.py
@@ -1,4 +1,6 @@
+from textual.app import ComposeResult
 from textual.containers import Container
+from textual.widgets import Static
 
 
 class Loader(Container):
@@ -11,12 +13,19 @@ class Loader(Container):
     }
     """
 
+    def __init__(self, *args, **kwargs):
+        self.label = Static(self.stops[self.index])
+        super().__init__(*args, **kwargs)
+
+    def compose(self) -> ComposeResult:
+        yield self.label
+
     def on_mount(self) -> None:
         self.auto_refresh = 1 / 10
 
-    def render(self):
-        value = self.stops[self.index]
+    def automatic_refresh(self):
         self.index += 1
         if self.index >= len(self.stops):
             self.index = 0
-        return value
+        value = self.stops[self.index]
+        self.label.update(value)
